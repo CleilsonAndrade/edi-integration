f6f4a3ad5ff94d6383bf1423ebc5b8161572a93c
CleilsonAndrade <cleilsonjose@hotmail.com>
2026-02-04 16:16:48 -0300

    feat: implement DatabaseConfigService for TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigService } from './config/database.config';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-
+      useClass: DatabaseConfigService,
     }),
 
     ScheduleModule.forRoot(),
---
+++ src/config/database.config.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class DatabaseConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      name: 'winthor_conn',
+      type: 'oracle',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 1521,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_SERVICE_NAME,
+
+      entities: [__dirname + '/../**/*.entity{.ts,.js'],
+
+      synchronize: false,
+
+      logging: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+
+      extra: {
+        poolMin: 2,
+        poolMax: 10,
+        poolIncrement: 1,
+        poolTImeout: 60,
+      },
+
+      migrations: [__dirname + '../database/migrations/*{.ts,.js}'],
+      migrationsRun: false,
+    }
+  }
+}
